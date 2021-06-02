4806cb1b123cac89ff174a7ada71887781d0e9f7
sunilsharma2214 <sunilsharma.2214@gmail.com>
2021-06-02 23:09:05 +0530

    Part 2:Solution

--- Restaurant_project+-Student+version/Restaurant_project -Student version/src/test/java/RestaurantServiceTest.java
+++ Restaurant_project+-Student+version/Restaurant_project -Student version/src/test/java/RestaurantServiceTest.java
@@ -56,6 +56,7 @@ class RestaurantServiceTest {
 
   @Test
   public void add_restaurant_should_increase_list_of_restaurants_size_by_1() {
+    int initialNumberOfRestaurants = service.getRestaurants().size();
     service.addRestaurant(
         "Pumpkin Tales", "Chennai", LocalTime.parse("12:00:00"), LocalTime.parse("23:00:00"));
     assertEquals(initialNumberOfRestaurants + 1, service.getRestaurants().size());
