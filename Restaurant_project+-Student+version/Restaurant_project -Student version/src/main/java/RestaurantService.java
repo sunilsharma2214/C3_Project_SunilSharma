6d53bf4c576c0ee597211c889b24330a72e0eda3
sunilsharma2214 <sunilsharma.2214@gmail.com>
2021-06-02 21:26:11 +0530

    Part 2:Solution

--- Restaurant_project+-Student+version/Restaurant_project -Student version/src/main/java/RestaurantService.java
+++ Restaurant_project+-Student+version/Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -3,32 +3,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantService {
-    private static final List<Restaurant> restaurants = new ArrayList<>();
+  private static final List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
-        Restaurant searchResult =  restaurants.stream()
-                .filter(restaurant -> restaurantName.equals(restaurant.getName()))
-                .findAny()
-                .orElse(null);
-        if(searchResult == null)
-         throw new restaurantNotFoundException("Restaurant not found");
-        else return searchResult;
-    }
+  public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException {
+    Restaurant searchResult =
+        restaurants.stream()
+            .filter(restaurant -> restaurantName.equals(restaurant.getName()))
+            .findAny()
+            .orElse(null);
+    if (searchResult == null) throw new restaurantNotFoundException("Restaurant not found");
+    else return searchResult;
+  }
 
+  public Restaurant addRestaurant(
+      String name, String location, LocalTime openingTime, LocalTime closingTime) {
+    Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+    restaurants.add(newRestaurant);
+    return newRestaurant;
+  }
 
-    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
-        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
-        restaurants.add(newRestaurant);
-        return newRestaurant;
-    }
+  public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+    Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+    restaurants.remove(restaurantToBeRemoved);
+    return restaurantToBeRemoved;
+  }
 
-    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
-        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
-        restaurants.remove(restaurantToBeRemoved);
-        return restaurantToBeRemoved;
-    }
-
-    public List<Restaurant> getRestaurants() {
-        return restaurants;
-    }
+  public List<Restaurant> getRestaurants() {
+    return restaurants;
+  }
 }
